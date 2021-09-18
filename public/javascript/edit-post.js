a55fb26ae0c0cdb942ffee4712cfb45237501898
Anthony Pena <apena5@alumni.jh.edu>
2021-09-18 17:34:08 -0400

    blog post full functionality

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -5,11 +5,14 @@ async function editFormHandler(event) {
     const id = window.location.toString().split('/')[
         window.location.toString().split('/').length - 1
     ];
+    const post_content = document.querySelector('textarea[name="post-content"]').value;
+
 
     let response = await fetch(`/api/posts/${id}`, {
         method: 'PUT',
         body: JSON.stringify({
-            title
+            title,
+            post_content
         }),
         headers: {
             'Content-Type': 'application/json'
