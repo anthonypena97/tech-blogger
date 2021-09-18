610b63f7a04a9abf3ff02a3664823003d0d88a5a
Anthony Pena <apena5@alumni.jh.edu>
2021-09-18 14:11:07 -0400

    initial file structure

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const routes = require('./controllers/');
+const sequelize = require('./config/connection');
+const path = require('path');
+const helpers = require('./utils/helpers');
+require('dotenv').config();
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: process.env.APP_SECRET,
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
+
+// HANDLEBARS ENGINE
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// EXPRESS TO SERVE JSON
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// USING THE PUBLIC FOLDER TO SERVE STATIC FILES
+app.use(express.static(path.join(__dirname, 'public')));
+
+// turn on routes
+app.use(routes);
+
+// turn on conection to db and server
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`Now Listening on ${PORT} `));
+});
