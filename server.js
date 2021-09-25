fde12ac72dd6fab45d5b32b893206d0b5d2bccaa
Anthony Pena <apena5@alumni.jh.edu>
2021-09-24 20:36:36 -0400

    heroku server bug

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 // turn on conection to db and server
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: true }).then(() => {
     app.listen(PORT, () => console.log(`Now Listening on ${PORT} `));
 });
 
